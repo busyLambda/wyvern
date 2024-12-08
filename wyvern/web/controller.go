5d7b9d06f20f1e6f8e0a88d328656e62fa0725d3
Susa Milán Mihály <mailtosnek@protonmail.com>
2024-12-08 08:29:43 +0100

    Controller stuff

--- wyvern/web/controller.go
+++ wyvern/web/controller.go
@@ -1,10 +1,13 @@
 package web
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/busyLambda/wyvern/wyvern"
 )
 
-func HandlerFromController(c Controller) func(w http.ResponseWriter, r *http.Request) {
+func HandlerFromController(c Controller) wyvern.Handler {
 	return c.Mount()
 }
 
@@ -13,14 +16,14 @@ type MountFunc = func(w *http.ResponseWriter, r *http.Request, res *Resources)
 func NewController(mountfunc MountFunc, res *Resources) Controller {
 	return Controller{
 		MountFunc:        mountfunc,
-		reactiveHandlers: make(map[string]func(w http.ResponseWriter, r *http.Request)),
+		reactiveHandlers: make(map[string]wyvern.Handler),
 		res:              res,
 	}
 }
 
 type Controller struct {
 	MountFunc        func(w *http.ResponseWriter, r *http.Request, res *Resources)
-	reactiveHandlers map[string]func(w http.ResponseWriter, r *http.Request)
+	reactiveHandlers map[string]wyvern.Handler
 	res              *Resources
 
 	// Handler(w http.ResponseWriter, r *http.Request)
@@ -28,7 +31,7 @@ type Controller struct {
 	// AttachReactiveHandler()
 }
 
-func (c *Controller) Mount() func(w http.ResponseWriter, r *http.Request) {
+func (c *Controller) Mount() wyvern.Handler {
 	return func(w http.ResponseWriter, r *http.Request) {
 		c.MountFunc(&w, r, c.res)
 	}
@@ -39,4 +42,18 @@ func (c *Controller) AttachReactiveHandler(name string, handler func(w http.Resp
 		handler(w, r, c.res)
 	}
 	c.reactiveHandlers[name] = finalHandler
+}
+
+func (c *Controller) SetRes(res *Resources) {
+	if c.res != nil {
+		c.res = res
+	} else {
+		warn := `
+		[!Warning!]: Attempted to manually set web.Controller.res using SetRes()
+		If you are not a developer of the library you shouldn't use this method
+		it is only meant to be used once.
+		`
+
+		fmt.Print(warn)
+	}
 }
